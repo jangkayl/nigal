6eb4d2361166fbd2f3df93b8d9804ee3287b304c
jangkayl <villarazojk@gmail.com>
2024-08-08 22:03:52 +0800

    main images fix

--- components/AllGoods.tsx
+++ components/AllGoods.tsx
@@ -3,41 +3,24 @@ import allGoodsData from "@/lib/sample-data";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-import Undermaintenance from "./detail/Undermaintenance";
 import { orderType } from "@/types";
 
 const AllGoods = () => {
-	const [isModalVisible, setIsModalVisible] = useState(false);
-	const [selectedOrder, setSelectedOrder] = useState<orderType | null>(null);
 	const router = useRouter();
 
 	const handleClick = (index: number) => {
-		if (index === 2 || index === 3) {
-			setIsModalVisible(true);
-		} else {
-			router.push(`/detail/${index + 1}`);
-		}
-	};
-
-	const handleCloseModal = () => {
-		setIsModalVisible(false);
-		setSelectedOrder(null);
+		router.push(`/detail/${index + 1}`);
 	};
 
 	return (
 		<div className="py-3 flex justify-center">
 			<div className="grid grid-cols-2 gap-3">
-				<Undermaintenance
-					isVisible={isModalVisible}
-					onClose={handleCloseModal}
-				/>
-
 				{allGoodsData.prices.map((item, index) => (
 					<button
 						key={index}
-						className="border shadow-md rounded-md"
+						className="border shadow-md rounded-md w-[10rem]"
 						onClick={() => handleClick(index)}>
-						<div className="h-auto w-full">
+						<div className="h-auto w-[full]">
 							<Image
 								src={item.image}
 								alt="promo"
